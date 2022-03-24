5dd5acd1ae3fcd60192b02576cae147e00fb69ae
Rafael Moro <rafael.moro.galindo@gmail.com>
2022-03-23 19:48:48 -0600

    [ADD] - The fetch of the data using promise all were successfull. It's missing to show that data

--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -2,6 +2,25 @@ import React from 'react'
 
 const useInitialState = () => {
     const API = process.env.API
-    console.log(API)
+    const [state, setState] = React.useState({
+        people: [],
+        films: [],
+        species: [],
+        planets: [],
+        starships: []
+    })
+    const urlAPIs = [`${API}people/`, `${API}films/`, `${API}species/`, `${API}planets/`, `${API}starships/`]
+    React.useEffect(() => {
+        Promise.all(urlAPIs.map(url => fetch(url)))
+        .then(results => Promise.all(results.map(response => response.json())))
+        .then((data) => {
+            setState({...state, people: data[0].results})
+            setState({...state, films: data[1].results})
+            setState({...state, species: data[2].results})
+            setState({...state, planets: data[3].results})
+            setState({...state, starships: data[4].results})
+        })
+    }, [])
+    return state
 }
 export {useInitialState}
